e3d71d7adbc4d4700e9eb4230fab5ab5820c806c
vletnaycnhyn <vletnyanchyn@sensoriainc.com>
2015-09-07 12:51:39 +0300

    Add DelegateCommand

--- HomeCalc.Model/BasicModels/ViewModel.cs
+++ HomeCalc.Model/BasicModels/ViewModel.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using HomeCalc.Core.Presentation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace HomeCalc.Model.BasicModels
 {
@@ -17,6 +20,12 @@ namespace HomeCalc.Model.BasicModels
             {
                 PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
             }
+        }
+
+        public bool TryGetMember(GetMemberBinder binder, out ICommand command)
+        {
+            command = new DelegateCommand();
+            return true;
         }
     }
 }
