f5c9e1bf533c1b6af125e2b26323389b3f38aa03
DmitriyBalykin <b_dmitriy82@mail.ru>
2015-09-20 21:58:02 +0300

    Add Purchase object and stylings

---
+++ HomeCalc.Model/DataModels/Purchase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeCalc.Presentation.DataModels
+{
+    public class Purchase
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public double TotalCost { get; set; }
+        public double ItemCost { get; set; }
+        public double ItemsNumber { get; set; }
+        public PurchaseType Type { get; set; }
+    }
+}
---
+++ HomeCalc.Model/ViewModels/SearchComboViewModel.cs
@@ -0,0 +1,40 @@
+﻿using HomeCalc.Core.LogService;
+using HomeCalc.Presentation.BasicModels;
+using HomeCalc.Presentation.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeCalc.Presentation.ViewModels
+{
+    public class SearchComboViewModel : ViewModel
+    {
+        public SearchComboViewModel()
+        {
+            logger = LogService.GetLogger();
+
+            List<PurchaseType> types = new List<PurchaseType>();
+            types.Add(new PurchaseType { Id = 0, Name = "Еда" });
+            types.Add(new PurchaseType { Id = 1, Name = "Хозяйственные товары" });
+            types.Add(new PurchaseType { Id = 2, Name = "Автомобиль" });
+            types.Add(new PurchaseType { Id = 3, Name = "Квартира" });
+            types.Add(new PurchaseType { Id = 4, Name = "Снаряжение" });
+
+            searchResultList = new List<Purchase>();
+           // searchResultList.Add(new Purchase { Id = 0, Ty });
+            
+        }
+
+        private List<Purchase> searchResultList;
+        public ObservableCollection<Purchase> SearchResultList
+        {
+            get
+            {
+                return new ObservableCollection<Purchase>(searchResultList);
+            }
+        }
+    }
+}
