029c55e3bff650d9b81e02d86dfe9c5320e39973
DmitriyBalykin <b_dmitriy82@mail.ru>
2016-01-08 00:59:19 +0200

    Add ChartsLib project

---
+++ HomeCalc.ChartsLib/ViewModels/AreaPlotViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeCalc.ChartsLib.ViewModels
+{
+    public class AreaPlotViewModel : UserControlViewModel
+    {
+        public AreaPlotViewModel()
+        {
+            HeaderHeight = 0;
+            FooterHeight = 0;
+            LeftLegendWidth = 0;
+            RightLegendWidth = 0;
+        }
+        public int HeaderHeight { get; set; }
+        public int FooterHeight { get; set; }
+        public int LeftLegendWidth { get; set; }
+        public int RightLegendWidth { get; set; }
+    }
+}
---
+++ HomeCalc.ChartsLib/ViewModels/UserControlViewModel.cs
@@ -0,0 +1,47 @@
+﻿//using HomeCalc.Core.LogService;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Dynamic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HomeCalc.ChartsLib.ViewModels
+{
+    public partial class UserControlViewModel : INotifyPropertyChanged
+    {
+        //protected Logger logger;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public UserControlViewModel()
+        {
+            //logger = new Logger(this.GetType().ToString());
+        }
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        protected void OnPropertyChanged<T>(Expression<Func<T>> property)
+        {
+            if (property == null)
+            {
+                return;
+            }
+            var memberExpression = property.Body as MemberExpression;
+            if (memberExpression == null)
+            {
+                return;
+            }
+            if (PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(memberExpression.Member.Name));
+            }
+        }
+    }
+}
