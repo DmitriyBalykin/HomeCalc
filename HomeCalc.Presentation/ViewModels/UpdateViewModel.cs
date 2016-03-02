bba4e149ee9491b9cfca0646e18bed96301c12f3
Dmytro Balykin <dbalkin@channeliq.com>
2016-03-02 23:41:53 +0200

    Add update control view and model

---
+++ HomeCalc.Presentation/ViewModels/UpdateViewModel.cs
@@ -0,0 +1,47 @@
+﻿using HomeCalc.Core.LogService;
+using HomeCalc.Core.Presentation;
+using HomeCalc.Presentation.BasicModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeCalc.Presentation.ViewModels
+{
+    public class UpdateViewModel : ViewModel
+    {
+        public UpdateViewModel()
+        {
+            logger = LogService.GetLogger();
+            AddCommand("CheckUpdates", new DelegateCommand(CheckUpdatesCommandExecute));
+            AddCommand("Update", new DelegateCommand(UpdateCommandExecute));
+        }
+
+        private void CheckUpdatesCommandExecute(object obj)
+        {
+            
+        }
+
+        private void UpdateCommandExecute(object obj)
+        {
+
+        }
+
+        private string versionChanges;
+        public string VersionChanges
+        {
+            get { return versionChanges; }
+            set
+            {
+                if (value != versionChanges)
+                {
+                    versionChanges = value;
+                    OnPropertyChanged(() => VersionChanges);
+                }
+            }
+        }
+        
+    }
+}
---
+++ HomeCalc.View/Controls/UpdateControl.xaml.cs
@@ -0,0 +1,19 @@
+﻿using HomeCalc.Presentation.ViewModels;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HomeCalc.View.Controls
+{
+    /// <summary>
+    /// Interaction logic for SettingsControl.xaml
+    /// </summary>
+    ///
+    public partial class UpdateControl : UserControl
+    {
+        public UpdateControl()
+        {
+            InitializeComponent();
+            this.DataContext = new UpdateViewModel();
+        }
+    }
+}
