73f7b486709b197d336fc80f4e53dfba1d4ddcef
Dmytro Balykin <dbalkin@channeliq.com>
2016-04-22 21:51:53 +0300

    Add Unit Tests skeleton

---
+++ HomeCalc.UnitTests/UnitTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HomeCalc.UnitTests
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestInitialize]
+        public void Initialization()
+        {
+
+        }
+        [TestMethod]
+        public void DefaultDBSchemaTest()
+        {
+            //check default DB schema
+        }
+
+        [TestMethod]
+        public void PurchaseTypeDBWriteReadTest()
+        {
+            //write purchase type
+            //read purchase type
+            //rename puchase type
+            //delete purchase type
+        }
+
+        [TestMethod]
+        public void PurchaseDBWriteReadTest()
+        {
+            //create purchase
+            //read purchase
+            //edit purchase name
+            //edit purchase cost
+            //edit purchase number
+            //edit purchase total cost
+            //delete purchase
+        }
+
+        [TestMethod]
+        public void SettingsDBWriteReadTest()
+        {
+            //read default settings
+            //change one by one settings values
+        }
+    }
+}
